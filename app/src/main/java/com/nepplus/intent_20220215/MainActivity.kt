85a43d25965e45c762a2df85d44c8b37dfb46c81
이윤주 <toyaann@naver.com>
2022-02-15 17:06:52 +0900

    받은 메세지를 보여줄 액티비티 별도로 추가

--- app/src/main/java/com/nepplus/intent_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220215/MainActivity.kt
@@ -14,8 +14,17 @@ class MainActivity : AppCompatActivity() {
 
 //            OtherActivity로 이동
             val myIntent = Intent( this, OtherActivity::class.java )
-
             startActivity(myIntent)
+
+        }
+
+        btnSendMessage.setOnClickListener {
+
+//            1. 뭐라고 입력했는지 추출
+
+
+
+//            2. 추출한 내용을 다른 화면에 전달하기 (화면으로 이동)
 
         }
 
